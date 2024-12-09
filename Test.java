640fb3aa82f124ac35f459ed8b1359fde107d2c7
ziyad372 <ziyadamjad372@gmail.com>
2024-12-09 14:05:10 +0500

    done

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+    public static double square(double a){
+
+        return Math. pow(a, 2);
+
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Square is: "+ square(4));
+        
+    }
+}
